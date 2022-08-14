417b60503e68dc6ab30306052540cb68ada3973d
christianlund <christian.lund0890@gmail.com>
2022-08-14 08:42:12 +0000

    create sendMail function for the contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,16 @@
-console.log("hello");
+function sendMail(contactForm) {
+    emailjs.send("service_y4v5x18", "template_lce1owa", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("Success!", response);
+        },
+        function(error) {
+            console.log("Failed!", error);
+        }
+    );
+    return false; // to block from loading a new page
+}
